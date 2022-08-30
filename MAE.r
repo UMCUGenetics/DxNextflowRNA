4ed7486b559301b43366afecd173dac4d62f9e69
Behzad Moumbeini <bmoumbeini@hpct01.op.umcutrecht.nl>
2022-08-30 12:14:57 +0200

    feature/MAE

--- MAE.r
+++ MAE.r
@@ -10,19 +10,19 @@ args<-commandArgs(TRUE)
 
 
 #import data
-mydata <- read.table(args[1], header=TRUE, sep="\t")
+data <- read.table(args[1], header=TRUE, sep="\t")
 
 
 # run MAE analysis
 
-		final <- resMAE <- DESeq4MAE(mydata, minCoverage = 10)
-	resMAE[, signif := padj < 0.05]
+final <- resMAE <- DESeq4MAE(data, minCoverage = 10)
+resMAE[, signif := padj < 0.05]
 resMAE[signif == TRUE, .N]
-	resMAE[, signif_ALT := signif == TRUE & altRatio >= 0.8]
-		resMAE[signif_ALT == TRUE, .N]
+resMAE[, signif_ALT := signif == TRUE & altRatio >= 0.8]
+resMAE[signif_ALT == TRUE, .N]
 
 #plot the data
-ggplot(mydata, aes(refCount+1, altCount+1)) + geom_point() +
+ggplot(data, aes(refCount+1, altCount+1)) + geom_point() +
     geom_abline(slope=1, intercept=0) + 
     scale_y_log10() + scale_x_log10() + theme_bw()
 
