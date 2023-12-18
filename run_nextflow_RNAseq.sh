f8fb218a16c394fcfd3bdda4fc7ec274a85559f1
“lonnekevanbrussel” <lonneke.vanbrussel@uvic.cat>
2023-12-18 10:53:45 +0100

    changed nextflow run command & order

--- run_nextflow_RNAseq.sh
+++ run_nextflow_RNAseq.sh
@@ -17,14 +17,15 @@ touch workflow.running
 export JAVA_HOME='/hpc/diaggen/software/tools/jdk-18.0.2.1/'  # change java version 
 export NXF_JAVA_HOME='/hpc/diaggen/software/tools/jdk-18.0.2.1/'  # change java vesion of nextflow
 
-var_nf=$(sbatch --mem 10G -t 01:00:00 -A diaggen --job-name Nextflow_RNA_testTG -e log/slurm_nextflow_rna_testTG.%j.err \
+#-export NONE
+#-profile slurm
+var_nf=$(sbatch --mem 10G -t 01:00:00 -A diaggen --job-name Nextflow_RNA_testTG -o log/slurm_nextflow_rna_testTG.%j.out -e log/slurm_nextflow_rna_testTG.%j.err \
+--nodes=1 --gres=tmpspace:10G --mail-user $email --mail-type FAIL \
 /hpc/diaggen/software/development/DxNextflowRNA/tools/nextflow run /hpc/diaggen/users/lonneke/github/DxNextflowRNA/test_main.nf  \
--c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/nextflow.config \
+-c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/nextflow.config -resume -ansi-log false \
 --input $input \
 --outdir $output \
 --email $email)
-#-profile slurm \
-#-resume -ansi-log false)
 
 echo "${var_nf}"
 echo "check directory for output: ${output}"
