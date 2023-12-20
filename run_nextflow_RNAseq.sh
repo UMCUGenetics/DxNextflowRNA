bd8963c2dec0ff7e513826911dac0a722363bf19
“lonnekevanbrussel” <lonneke.vanbrussel@uvic.cat>
2023-12-20 09:20:56 +0100

    added EOT and SBATCH commands

--- run_nextflow_RNAseq.sh
+++ run_nextflow_RNAseq.sh
@@ -13,22 +13,46 @@ mkdir -p log
 
 if ! { [ -f 'workflow.running' ] || [ -f 'workflow.done' ] || [ -f 'workflow.failed' ]; }; then
 touch workflow.running
+echo "check directory for output: ${output}"
 
 export JAVA_HOME='/hpc/diaggen/software/tools/jdk-18.0.2.1/'  # change java version 
 export NXF_JAVA_HOME='/hpc/diaggen/software/tools/jdk-18.0.2.1/'  # change java vesion of nextflow
 
-#-export NONE
-#-profile slurm
-var_nf=$(sbatch --mem 10G -t 01:00:00 -A diaggen --job-name Nextflow_RNA_testTG -o log/slurm_nextflow_rna_testTG.%j.out -e log/slurm_nextflow_rna_testTG.%j.err \
---nodes=1 --gres=tmpspace:10G --mail-user $email --mail-type FAIL \
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --mem 10G
+#SBATCH --gres=tmpspace:10G
+#SBATCH --job-name Nextflow_WES
+#SBATCH -o log/slurm_nextflow_wes.%j.out
+#SBATCH -e log/slurm_nextflow_wes.%j.err
+#SBATCH --mail-user $email
+#SBATCH --mail-type FAIL
+#SBATCH --account=diaggen
+
 /hpc/diaggen/software/development/DxNextflowRNA/tools/nextflow run /hpc/diaggen/users/lonneke/github/DxNextflowRNA/test_main.nf  \
--c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/nextflow.config -resume -ansi-log false \
+-c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/nextflow.config -resume -ansi-log false -profile slurm \
 --input $input \
 --outdir $output \
---email $email)
+--email $email
+ 
+if [ \$? -eq 0 ]; then
+    echo "Nextflow done."
 
-echo "${var_nf}"
-echo "check directory for output: ${output}"
+    echo "RNA Trimgalore test workflow completed successfully."
+    rm workflow.running
+    touch workflow.done
+
+    exit 0
+else
+    echo "Nextflow failed"
+    rm workflow.running
+    touch workflow.failed
+
+    exit 1
+fi
+EOT
 else
 echo "Workflow job not submitted, please check $output for 'workflow.status' files."
 fi
