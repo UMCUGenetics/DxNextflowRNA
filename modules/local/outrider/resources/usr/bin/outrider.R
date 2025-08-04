82f4f6842cb8f4d1b71893c88d65f2472af00305
Jorisvansteenbrugge <7196110+Jorisvansteenbrugge@users.noreply.github.com>
2025-08-04 13:59:45 +0200

    Improve filtering options

--- modules/local/outrider/resources/usr/bin/outrider.R
+++ modules/local/outrider/resources/usr/bin/outrider.R
@@ -1,76 +1,70 @@
 #!/usr/bin/env Rscript
 
-library(OUTRIDER)
-library(dplyr)
-library(purrr)
-library(magrittr)
-library(readr)
-library(argparse)
+suppressPackageStartupMessages(library(OUTRIDER))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(BiocParallel))
+
+
 
 parser <- ArgumentParser(description = "Run OUTRIDER")
-parser$add_argument(
-    "-q", "--queries",
-    metavar="query_files",
-    nargs="+",
-    help="Files containing the query input files."
-)
-parser$add_argument(
-    "-o", "--output_path",
-    metavar="output_path",
-    help="Path where output will be stored.",
-    default="./"
-)
-parser$add_argument(
-    "-r", "--ref",
-    metavar="reference_files",
-    nargs="+",
-    help="Files containing the reference input files."
-)
-parser$add_argument(
-    "-m", "--mode",
-    metavar="mode",
-    help="Running mode, gene or exon",
-    default="gene"
-)
-parser$add_argument(
-    "-g", "--gtf",
-    metavar="genome_gtf",
-    help="Genome gtf file"
-)
-parser$add_argument(
-    "-t", "--threads",
-    metavar="threads",
-    default=12,
-    help="Number of parallel threads",
-    type="integer"
-    )
-parser$add_argument(
-    "-p", "--prefix",
-    metavar="prefix",
-    help="Number of parallel threads"
-)
-parser$add_argument(
-  "-v", "--version",
-  action="version",
-  version=as.character(packageVersion("OUTRIDER"))
-)
+parser$add_argument("-q", "--queries", metavar="query_files", nargs="+",
+                    help="Files containing the query input files.")
+
+parser$add_argument("-o", "--output_path", metavar="output_path", default="./"
+                    help="Path where output will be stored.")
+
+parser$add_argument("-r", "--ref", metavar="reference_files", nargs="+",
+                    help="Files containing the reference input files.")
+
+parser$add_argument("-m", "--mode", metavar="mode", default="gene",
+                    help="Running mode, gene or exon")
+
+parser$add_argument("-g", "--gtf", metavar="genome_gtf",
+                    help="Genome gtf file")
+
+parser$add_argument( "-t", "--threads", metavar="threads", default=12, type="integer",
+                    help="Number of parallel threads")
+
+parser$add_argument("-p", "--prefix", metavar="prefix",
+                    help="Number of parallel threads")
+
+parser$add_argument("--filter_mode", choices=c("minCounts","fpkm"), default="minCounts",
+                    help=paste(c("Gene/Exon filter mode. minCounts filters only genes/exons with zero expression,",
+                                 "whereas fpkg filters also lowly epxressed genes/exons")))
+
+parser$add_argument("--fpkm_cutoff", type="double", default=1.0,
+                    help="Only used when --filter_mode is fpkm.")
+
+parser$add_argument("--fpkm_percentile", type="double", default=0.95,
+                    help="Only used when --filter_mode is fpkm.")
+
+parser$add_argument("-v", "--version", action="version", version=as.character(packageVersion("OUTRIDER")))
 
 args <- parser$parse_args()
 
 
-run_outrider <- function(count_matrix, metadata, mode, gtf, threads){
+run_outrider <- function(count_matrix, metadata, args){
 
   ods <- OutriderDataSet(countData = count_matrix)
 
-  if(mode == "gene"){
-    ods <- filterExpression(
-      ods,
-      fpkmCutoff=1,
-      minCounts=TRUE,
-      filterGenes=TRUE,
-      gtfFile=gtf
-    )
-  } else if(mode == "exon") {
+  if(args$modemode == "gene"){
+      if( args$filter_mode == "minCounts"){
+          ods <- filterExpression(ods, minCounts=TRUE, filterGenes=TRUE)
+      } else {
+        ods <- filterExpression(
+          ods,
+          fpkmCutoff = args$fpkm_cutoff,
+          percentile = args$fpkm_percentile,
+          minCounts = FALSE,
+          filterGenes = TRUE,
+          gtfFile = args$gtf)
+      }
+
+  } else if(args$mode == "exon") {
     mcols(ods)$basepairs <- metadata$Length # add exon length to the outrider dataset to ensure FPKM is correctly estimated
     ods <- filterExpression(
       ods,
@@ -80,25 +74,13 @@ run_outrider <- function(count_matrix, metadata, mode, gtf, threads){
     )
   }
 
+  # Runs the outrider pipeline in parallel
   ods <- OUTRIDER(
     ods,
     BPPARAM=MulticoreParam(threads)
   )
 
   return(ods)
-}
-
-make_query_plots <- function(query_files, ods, prefix, outdir = "./"){
-  volcano_dir <- paste0(outdir, prefix, "_volcano_plots/")
-  dir.create(volcano_dir, showWarnings = FALSE)
-
-  for (query in query_files){
-    file_name <- paste0(volcano_dir, query, ".png")
-    message(paste("Plotting volcano for: ", file_name, "In:", volcano_dir ))
-    pdf(file_name)
-    print(plotVolcano(ods, sampleID=query, basePlot=T))
-    dev.off()
-  }
 }
 
 read_and_split_counts <- function(file) {
@@ -134,8 +116,7 @@ main <- function(args){
   res_full <- results(ods, all=TRUE)
   res_signif <- results(ods, all=FALSE)
 
-  make_query_plots(args$queries, ods, prefix=args$prefix)
-
+  # Output both the unfiltered results, as the filtered results
   write.table(
     res_full,
     quote=F, sep="\t", row.names=F,
@@ -149,4 +130,6 @@ main <- function(args){
 
 }
 
+set.seed(1)
+bp <- MulticoreParam(args$threads, RNGseed=123)
 main(args)
